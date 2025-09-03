436e24a447d3d46c72cf96d3ef2b9a2cc2bab320
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-09-03 13:31:58 +0500

    [3] - created entity model of a Movie

---
+++ src/main/java/com/app/entity/Movie.java
@@ -0,0 +1,30 @@
+package com.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private int duration;
+    private String posterUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

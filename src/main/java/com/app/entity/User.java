02aeedf4dccc39dc30154db64334ade9e4457a19
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-09-03 13:31:24 +0500

    [3] - created entity model of an User

---
+++ src/main/java/com/app/entity/User.java
@@ -0,0 +1,30 @@
+package com.app.entity;
+
+import com.app.entity.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

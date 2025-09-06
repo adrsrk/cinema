745b83c40549b108124ea3bc5d92826981d04a11
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-09-07 02:12:28 +0500

    [4] - added global exception handler for the movie controller

---
+++ src/main/java/com/app/controller/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.app.controller;
+
+import com.app.exception.MovieNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleMovieNotFound(MovieNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", e.getMessage()));
+    }
+}

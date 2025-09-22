bd7c5ae3b555134dcc06261b726df82d99fca83a
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-09-23 00:58:02 +0500

    [8] - created repository classes for hall and session entity classes

---
+++ src/main/java/com/app/repository/HallRepository.java
@@ -0,0 +1,9 @@
+package com.app.repository;
+
+import com.app.entity.Hall;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HallRepository extends JpaRepository<Hall, Long> {
+}
---
+++ src/main/java/com/app/repository/SessionRepository.java
@@ -0,0 +1,9 @@
+package com.app.repository;
+
+import com.app.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+}

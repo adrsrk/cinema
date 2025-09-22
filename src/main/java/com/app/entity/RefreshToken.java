87b4af1285767dfd03b4f945e7d5bff9e2c2e70d
Serikbay Aidar <serikbay.aidar@gmail.com>
2025-09-23 00:43:35 +0500

    [6] - created entity class for refresh token

---
+++ src/main/java/com/app/entity/RefreshToken.java
@@ -0,0 +1,24 @@
+package com.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    private Instant expiresAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
